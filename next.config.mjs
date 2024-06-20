d805676b7625675ba9578363e3e1242f0ce08873
AuraGuardian23 <122215062+AuraGuardian23@users.noreply.github.com>
2024-06-20 20:38:58 +0500

    configure images design on phone cover for preview (partial)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+import { hostname } from 'os';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        // used for images that are hosted on upload thing
+        // domains: ["utfs.io"], // console says deprecated, use remotePatterns instead
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'utfs.io'
+            },
+        ],
+    }
+};
 
 export default nextConfig;
--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,6 +1,38 @@
-const Page = async () => {
+import { db } from "@/db"
+import { notFound } from "next/navigation"
+import DesignConfigurator from "./DesignConfigurator"
+
+// search params can be 0, 1 or more
+interface PageProps {
+    searchParams: {
+        [key: string]: string | string[] | undefined
+    }
+}
+
+const Page = async ({searchParams}: PageProps) => {
+    // id retrieved from search params
+    const {id} = searchParams
     // make db call
-    return <p></p>
+    if (!id || typeof id !== "string") {
+        return notFound()
+    }
+
+    const configuration = await db.configuration.findUnique({
+        where : { id }
+    })
+
+    if (!configuration) {
+        return notFound()
+    }
+
+    const { imageUrl, width, height } = configuration
+    
+    // return <DesignConfigurator imageUrl={imageUrl} configId={id} imageDimensions={{width:width, height:height}} />
+    return <DesignConfigurator
+                imageUrl={imageUrl}
+                configId={id}
+                imageDimensions={{width, height}}
+            />
 }
 
 export default Page
